9a5779eff4d753f34f170a7f3510b66b1731fa66
Mayur Kadam <mayurkadam@Mayurs-MacBook-Air.local>
2025-01-31 11:46:56 +0530

    fix of optimal chrome window width

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ chrome.action.onClicked.addListener(() => {
         const screenHeight = primaryDisplay.workArea.height;
 
         // Set popup window dimensions with minimum and maximum constraints
-        const popupWidth = Math.min(600, screenWidth * 0.8); // 80% of screen width up to 500px
+        const popupWidth = Math.min(580, screenWidth * 0.8); // 80% of screen width up to 500px
         let popupHeight = 675; // Your optimal height
 
         // Adjust height if screen is too small
